f97a08bbd5ca0c383b919ee8ab1cf71ac7808d66
oleksiikoval <lexi4.koval@gmail.com>
2023-05-23 10:11:49 -0400

    Security part1

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -18,38 +18,48 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//
+//        List<UserDetails> userList = new ArrayList<>();
+//
+//        userList.add(
+//                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
+//        );
+//        userList.add(
+//        new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
+//        );
+//
+//        return new InMemoryUserDetailsManager(userList);
+//    }
 
-        List<UserDetails> userList = new ArrayList<>();
 
-        userList.add(
-                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
-        );
-        userList.add(
-        new User("ozzy", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER")))
-        );
-
-        return new InMemoryUserDetailsManager(userList);
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests()
-                .antMatchers("/user/**").hasRole("ADMIN")
-                .antMatchers("/project/**").hasRole("MANAGER")
-                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
-                .antMatchers("/task/**").hasRole("MANAGER")
-                .antMatchers("/",
+                .antMatchers("/user/**").hasAnyAuthority("Admin")
+                //.antMatchers("/user/**").hasRole("ADMIN")
+//                .antMatchers("/project/**").hasRole("MANAGER")
+//                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+//                .antMatchers("/task/**").hasRole("MANAGER")
+                .antMatchers(
+                        "/",
                         "/login",
                         "/fragments/**",
-                        "assets/**",
-                        "/images/**"
+                        "/assets/**",
+                        "/images/**",
+                        "/welcome"
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic()
+//                .httpBasic()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/welcome")
+                .failureUrl("/login?error=true")
+                .permitAll()
                 .and().build();
     }
 }
